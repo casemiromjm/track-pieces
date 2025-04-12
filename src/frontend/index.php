1fb391efc17e8f89b4f74bce49fa3c03f72d6d24
casemiromjm <casemiromelojorge@gmail.com>
2025-04-12 19:45:13 +0100

    add templates and updated home

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -9,7 +9,11 @@
     <form method="POST" action="./includes/generate_qrcode.php">
         <button type="submit" name="generate_qrcode">Gerar Qrcode</button>
     </form>
+    <form method="POST" action="">
         <button type="submit" name="add_piece">Adicionar peça</button>
+    </form>
+    <form method="POST" action="">
         <button type="submit" name="search_piece">Buscar peça</button>
+    </form≳
 </body>
 </html>
