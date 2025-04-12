fb160a211a4e6e269cd5e540e75712e54f78f273
casemiromjm <casemiromelojorge@gmail.com>
2025-04-12 15:05:08 +0100

    initial logic for interface

---
+++ src/frontend/includes/generate_qrcode.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "generated qrcode";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Almoxarifado</title>
+</head>
+<body>
+    <a href="../index.php">Go back</a>
+</body>
+</html>
---
+++ src/frontend/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Almoxarifado</title>
+</head>
+<body>
+    <form method="POST" action="./includes/generate_qrcode.php">
+        <button type="submit" name="generate_qrcode">Gerar Qrcode</button>
+    </form>
+        <button type="submit" name="add_piece">Adicionar peça</button>
+        <button type="submit" name="search_piece">Buscar peça</button>
+</body>
+</html>
