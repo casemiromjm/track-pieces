eb15f57d94b15ce92e082bdaf51a84fe6a04e7e0
casemiromjm <casemiromelojorge@gmail.com>
2025-04-15 12:28:58 +0100

    templates now functioning

--- src/frontend/includes/generate_qrcode.php
+++ src/frontend/includes/generate_qrcode.php
@@ -1,17 +1,10 @@
 <?php
-
     echo "generated qrcode";
-
+    require_once("../template/header.html")
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de Almoxarifado</title>
-</head>
-<body>
-    <a href="../index.php">Go back</a>
-</body>
-</html>
+<a href="../index.php">Voltar para o início</a>
+
+<?php
+require_once("../template/footer.html")
+?>
--- src/frontend/index.php
+++ src/frontend/index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de Almoxarifado</title>
-</head>
-<body>
+<?php
+    $page_title = "Controle de Almoxarifado";
+    require_once("./template/header.html");
+?>
+<!-- header included -->
     <form method="POST" action="./includes/generate_qrcode.php">
         <button type="submit" name="generate_qrcode">Gerar Qrcode</button>
     </form>
@@ -14,6 +11,9 @@
     </form>
     <form method="POST" action="">
         <button type="submit" name="search_piece">Buscar peça</button>
-    </form≳
-</body>
-</html>
+    </form>
+
+<!-- footer included -->
+<?php
+    require_once("./template/footer.html");
+?>
